31ccd0edbd3670699bccaabefef111206fd8bbff
hajarbakir <hajar.bakir@docc.techstarter.de>
2024-03-04 15:33:41 +0100

    Update App.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,48 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "node": true,
-    "jest": true  // "jest" direkt hier hinzufügen
+    "jest/globals": true
   },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
   ],
-  "settings": {
-    "react": {
-      "version": "latest" // Hier die gewünschte React-Version angeben
-    }
-  },
   "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
   "plugins": [
-    "react"
+    "react", "jest"
   ],
   "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "semi": ["error", "never"],
+    "indent": [
+      "error",
+        2
+      ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", {"before": true, "after": true}],
-    "no-console": "off", // Sie können dies auf "error" setzen, wenn Sie Konsolenmeldungen nicht erlauben möchten
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "error",
     "react/prop-types": 0
   }
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { createRoot } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-const root = createRoot(document.getElementById('app')) // Änderung hier von 'root' zu 'app'
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('app'))
